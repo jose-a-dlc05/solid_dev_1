7e92afd084a5634a33f3667de0215b3f810c52b0
Jose De La Cruz <jose.delcruz0490@icloud.com>
2024-03-06 02:55:12 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+class ChickFilARestaurant {
+	private menuItems: string[] = [];
+	private orders: { customerName: string; items: string[] }[] = [];
+	private employees: { name: string; position: string }[] = [];
+
+	constructor() {}
+
+	public addMenuItem(item: string): void {
+		this.menuItems.push(item);
+	}
+
+	public removeMenuItem(item: string): void {
+		this.menuItems = this.menuItems.filter((menuItem) => menuItem !== item);
+	}
+
+	public logMenu(): string[] {
+		return this.menuItems;
+	}
+
+	public placeOrder(customerName: string, items: string[]): void {
+		this.orders.push({ customerName, items });
+	}
+
+	public completeOrder(customerName: string): void {
+		this.orders = this.orders.filter(
+			(order) => order.customerName !== customerName
+		);
+	}
+
+	public hireEmployee(name: string, position: string): void {
+		this.employees.push({ name, position });
+	}
+
+	public fireEmployee(name: string): void {
+		this.employees = this.employees.filter(
+			(employee) => employee.name !== name
+		);
+	}
+}
+
+const chickFilARestaurantHandler = new ChickFilARestaurant();
+chickFilARestaurantHandler.addMenuItem('Deluxe Chicken Sandwich');
+console.log(chickFilARestaurantHandler.logMenu());
